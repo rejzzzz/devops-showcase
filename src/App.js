f3898da1162c14fe6d6dd9c1f58c5b149c658c86
Rejwanul Hoque <hoquerejwanulrh@gmail.com>
2024-09-16 19:14:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DevOps Project Showcase</h1>
+        <p>
+          Welcome to my DevOps project showcase! Here are the details of the project:
+        </p>
+        <div>
+          <h2>Project Overview</h2>
+          <p>This project involves setting up a CI/CD pipeline using Jenkins, Docker, and Kubernetes.</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
